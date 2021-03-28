8885d7daf39ce843f01f4c0f6f9fb35a79374d82
nmurata <nmurata73@gmail.com>
2021-03-29 02:00:50 +0900

    generate image-color-picker module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './modules/material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonService } from './services/common.service';
