146b0f52c5a6f0b9a0301968d1aa1c917bd487be
nmurata <nmurata73@gmail.com>
2021-03-29 02:21:49 +0900

    generate image-color-picker component

---
+++ src/app/screens/image-color-picker/components/image-color-picker/image-color-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-color-picker',
+  templateUrl: './image-color-picker.component.html',
+  styleUrls: ['./image-color-picker.component.scss'],
+})
+export class ImageColorPickerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/screens/image-color-picker/image-color-picker.module.ts
+++ src/app/screens/image-color-picker/image-color-picker.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Routes, RouterModule } from '@angular/router';
 import { MaterialModule } from '../../modules/material/material.module';
+import { ImageColorPickerComponent } from './components/image-color-picker/image-color-picker.component';
+
+const routes: Routes = [{ path: '', component: ImageColorPickerComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, MaterialModule],
+  declarations: [ImageColorPickerComponent],
+  imports: [RouterModule.forChild(routes), CommonModule, MaterialModule],
 })
 export class ImageColorPickerModule {}
