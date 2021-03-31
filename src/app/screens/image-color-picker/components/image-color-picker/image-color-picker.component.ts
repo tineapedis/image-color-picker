a79925680decdea55e720ff4b8d81adf3fbe044f
nmurata <nmurata73@gmail.com>
2021-03-31 23:07:05 +0900

    dark theme image color picker

--- src/app/screens/image-color-picker/components/image-color-picker/image-color-picker.component.ts
+++ src/app/screens/image-color-picker/components/image-color-picker/image-color-picker.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CommonService } from '../../../../services/common.service';
 
 @Component({
   selector: 'app-image-color-picker',
   templateUrl: './image-color-picker.component.html',
   styleUrls: ['./image-color-picker.component.scss'],
 })
-export class ImageColorPickerComponent implements OnInit {
-  constructor() {}
+export class ImageColorPickerComponent implements OnInit, OnDestroy {
+  isLight = true;
+  private subscription!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private commonService: CommonService) {}
+
+  ngOnInit() {
+    this.subscription = this.commonService.isLightObserver$.subscribe(
+      (isLight) => {
+        this.isLight = isLight;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
