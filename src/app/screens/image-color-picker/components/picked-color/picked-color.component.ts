e23f11469bea4c6419584130e5cc94074a9f0a6b
nmurata <nmurata73@gmail.com>
2021-04-04 22:27:35 +0900

    generate picked-color-component

---
+++ src/app/screens/image-color-picker/components/picked-color/picked-color.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-picked-color',
+  templateUrl: './picked-color.component.html',
+  styleUrls: ['./picked-color.component.scss'],
+})
+export class PickedColorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
