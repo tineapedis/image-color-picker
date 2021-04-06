ba79db1c32f80566a046acfd910a32f971a0fb15
nmurata <nmurata73@gmail.com>
2021-04-07 03:27:03 +0900

    refactor

--- src/app/screens/image-color-picker/components/image-display/image-display.component.ts
+++ src/app/screens/image-color-picker/components/image-display/image-display.component.ts
@@ -10,6 +10,8 @@ export class ImageDisplayComponent implements OnInit, AfterViewInit {
   @Output() eventSelectRGB = new EventEmitter<RGB>();
   @Output() eventPointerRGB = new EventEmitter<RGB>();
   private canvas?: HTMLCanvasElement;
+  private windowWidth = 0;
+  private image = new Image();
 
   constructor() {}
 
@@ -17,8 +19,15 @@ export class ImageDisplayComponent implements OnInit, AfterViewInit {
     this.canvas = document.getElementById('image-canvas') as HTMLCanvasElement;
 
     window.onresize = () => {
+      if (!this.canvas || window.innerWidth === this.windowWidth) {
+        return;
+      }
+      this.canvas.width = window.innerWidth * 0.8;
+      this.windowWidth = window.innerWidth;
       this.drawImage();
     };
+
+    this.setUpMouseEvent();
   }
 
   ngAfterViewInit() {
@@ -26,20 +35,23 @@ export class ImageDisplayComponent implements OnInit, AfterViewInit {
       return;
     }
 
-    const context = this.canvas.getContext('2d');
     const containerPickerColor = document.getElementById(
       'container-picker-color'
     );
 
     this.canvas.width = window.innerWidth * 0.8;
+    this.windowWidth = window.innerWidth;
     this.canvas.height =
-      containerPickerColor != null ? containerPickerColor.clientHeight : 0;
-
-    if (context == null) {
-      return;
-    }
+      containerPickerColor === null ? 0 : containerPickerColor.clientHeight;
 
     this.drawImage();
+  }
+
+  private setUpMouseEvent() {
+    const context = this.canvas?.getContext('2d');
+    if (!this.canvas || !context) {
+      return;
+    }
 
     this.canvas.onclick = (evt: MouseEvent) => {
       const color = this.extractColor(context, evt);
@@ -52,55 +64,53 @@ export class ImageDisplayComponent implements OnInit, AfterViewInit {
     };
   }
 
-  drawImage() {
+  private drawImage() {
     const context = this.canvas?.getContext('2d');
-    const img = new Image();
 
-    img.onload = () => {
+    this.image.onload = () => {
       if (!context) {
         return;
       }
 
-      const canvasAspect = context.canvas.width / context.canvas.height; // canvasのアスペクト比
-      const imgAspect = img.width / img.height; // 画像のアスペクト比
+      const canvas = context.canvas;
+      const canvasAspect = canvas.width / canvas.height;
+      const imageAspect = this.image.width / this.image.height;
       let left = 0;
       let top = 0;
       let width = 0;
       let height = 0;
 
-      if (imgAspect >= canvasAspect) {
-        // 画像が横長
-        left = 0;
-        width = context.canvas.width;
-        height = context.canvas.width / imgAspect;
-        top = (context.canvas.height - height) / 2;
+      if (imageAspect >= canvasAspect) {
+        // 画像横長
+        width = canvas.width;
+        height = canvas.width / imageAspect;
+        top = (canvas.height - height) / 2;
       } else {
-        // 画像が縦長
-        top = 0;
-        height = context.canvas.height;
-        width = context.canvas.height * imgAspect;
-        left = (context.canvas.width - width) / 2;
+        // 画像縦長
+        height = canvas.height;
+        width = canvas.height * imageAspect;
+        left = (canvas.width - width) / 2;
       }
       context.drawImage(
-        img,
+        this.image,
         0,
         0,
-        img.width,
-        img.height,
+        this.image.width,
+        this.image.height,
         left,
         top,
         width,
         height
       );
     };
-    img.src = '/assets/images/techi.jpeg';
+    this.image.src = '/assets/images/techi.jpeg';
   }
 
   private extractColor(
     context: CanvasRenderingContext2D,
-    evt: MouseEvent
+    event: MouseEvent
   ): RGB {
-    const imagedata = context.getImageData(evt.offsetX, evt.offsetY, 1, 1);
+    const imagedata = context.getImageData(event.offsetX, event.offsetY, 1, 1);
     return {
       red: imagedata.data[0],
       green: imagedata.data[1],
