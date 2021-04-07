a772abf44b3d716018d1f156e9b5fbf3a6638060
nmurata <nmurata73@gmail.com>
2021-04-08 02:38:20 +0900

    add MatTooltipModule

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
@@ -14,9 +13,8 @@ import { MatCardModule } from '@angular/material/card';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatCardModule,
+    MatTooltipModule,
   ],
 })
 export class MaterialModule {}
