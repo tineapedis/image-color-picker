5496e17dc21d85d2a7900da4e346d11706679968
nmurata <nmurata73@gmail.com>
2021-04-08 22:41:57 +0900

    generate image-select component

---
+++ src/app/screens/image-color-picker/components/image-select/image-select.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-select',
+  templateUrl: './image-select.component.html',
+  styleUrls: ['./image-select.component.scss'],
+})
+export class ImageSelectComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
