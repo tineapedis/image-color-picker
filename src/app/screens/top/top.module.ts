693a096ce9fd7b31fd20b35e12608404ebad3a60
nmurata <nmurata73@gmail.com>
2021-05-04 14:20:07 +0900

    generate top-main component

--- src/app/screens/top/top.module.ts
+++ src/app/screens/top/top.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopMainComponent } from './components/top-main/top-main.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TopMainComponent],
   imports: [CommonModule],
 })
 export class TopModule {}
