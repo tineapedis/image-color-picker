acbc6f22856db405907e869b65d818b524b8d21e
nmurata <nmurata73@gmail.com>
2021-05-07 00:52:34 +0900

    impl color tools component layout

--- src/app/screens/top/components/color-tools/color-tools.component.ts
+++ src/app/screens/top/components/color-tools/color-tools.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-tools.component.scss'],
 })
 export class ColorToolsComponent implements OnInit {
-  tests = ['test', 'test2', 'test3', 'test4'];
+  tests = ['Picker', 'test2', 'test3', 'test4'];
   subs = ['sub', 'sub2', 'sub3'];
 
   constructor() {}
--- src/app/screens/top/top.module.ts
+++ src/app/screens/top/top.module.ts
@@ -4,11 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { TopComponent } from './components/top/top.component';
 import { ColorToolsComponent } from './components/color-tools/color-tools.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../../modules/material/material.module';
 
 const routes: Routes = [{ path: '', component: TopComponent }];
 
 @NgModule({
   declarations: [TopComponent, ColorToolsComponent],
-  imports: [RouterModule.forChild(routes), CommonModule, FlexLayoutModule],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
+  ],
 })
 export class TopModule {}
