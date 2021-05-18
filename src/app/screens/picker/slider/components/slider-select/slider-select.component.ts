630f75286b172fbaac3a2d5e880f8e7874a74f12
nmurata <nmurata73@gmail.com>
2021-05-19 00:52:19 +0900

    generate slider-select component

---
+++ src/app/screens/picker/slider/components/slider-select/slider-select.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-select',
+  templateUrl: './slider-select.component.html',
+  styleUrls: ['./slider-select.component.scss'],
+})
+export class SliderSelectComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/screens/picker/slider/slider.module.ts
+++ src/app/screens/picker/slider/slider.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule } from '../../../modules/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SliderComponent } from './components/slider/slider.component';
+import { SliderSelectComponent } from './components/slider-select/slider-select.component';
 
 const routes: Routes = [{ path: '', component: SliderComponent }];
 
 @NgModule({
-  declarations: [SliderComponent],
-  imports: [RouterModule.forChild(routes), CommonModule],
+  declarations: [SliderComponent, SliderSelectComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
 })
 export class SliderModule {}
