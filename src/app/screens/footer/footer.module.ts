0dc7b6a858fe669a6389b06d4ac5e327547710fe
nmurata <nmurata73@gmail.com>
2021-06-20 18:37:32 +0900

    generate cotact component

--- src/app/screens/footer/footer.module.ts
+++ src/app/screens/footer/footer.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ContactComponent } from './components/contact/contact.component';
+
+const routes: Routes = [{ path: 'contact', component: ContactComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [ContactComponent],
+  imports: [RouterModule.forChild(routes), CommonModule],
 })
 export class FooterModule {}
