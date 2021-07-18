bea2ffa588371d0ea272647bebd10dfc15cb7c91
nmurata <nmurata73@gmail.com>
2021-07-19 00:10:30 +0900

    show hsl value

--- src/app/screens/chart/components/saturation/saturation.component.ts
+++ src/app/screens/chart/components/saturation/saturation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-saturation',
@@ -6,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saturation.component.scss'],
 })
 export class SaturationComponent implements OnInit {
-  constructor() {}
+  colorService: ColorService;
+  hexList: string[] = [];
+  hsl: HSL;
+
+  constructor(colorService: ColorService) {
+    this.colorService = colorService;
+    this.hsl = colorService.convertRgbToHsl();
+
+    const MAX = 24;
+    for (let i = 0; i < MAX; i++) {
+      const saturation = i * (100 / MAX);
+      const rgb = this.colorService.convertHslToRgb({
+        hue: this.hsl.hue,
+        saturation,
+        lightness: this.hsl.lightness,
+      });
+      this.hexList.push(this.colorService.convertRgbToHex(rgb));
+    }
+  }
 
   ngOnInit(): void {}
 }
