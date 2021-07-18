19b38d4b3eda4bd5666f1b00330ed9d528975903
nmurata <nmurata73@gmail.com>
2021-07-18 21:30:23 +0900

    generate saturation component

---
+++ src/app/screens/chart/components/saturation/saturation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saturation',
+  templateUrl: './saturation.component.html',
+  styleUrls: ['./saturation.component.scss'],
+})
+export class SaturationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
