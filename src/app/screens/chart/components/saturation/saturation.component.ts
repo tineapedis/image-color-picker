cf033e3c1f923fc9005ea39754b4da0831d342f9
nmurata <nmurata73@gmail.com>
2021-07-20 03:12:45 +0900

    impl selectNumber

--- src/app/screens/chart/components/saturation/saturation.component.ts
+++ src/app/screens/chart/components/saturation/saturation.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ColorService } from 'src/app/services/color.service';
 
+interface ChartNumber {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-saturation',
   templateUrl: './saturation.component.html',
@@ -9,13 +14,34 @@ import { ColorService } from 'src/app/services/color.service';
 export class SaturationComponent implements OnInit {
   colorService: ColorService;
   hexList: string[] = [];
+
+  selected = '12';
+  chartNumbers: ChartNumber[] = [
+    { value: '8', viewValue: '8' },
+    { value: '12', viewValue: '12' },
+    { value: '16', viewValue: '16' },
+    { value: '20', viewValue: '20' },
+    { value: '24', viewValue: '24' },
+    { value: '28', viewValue: '28' },
+    { value: '32', viewValue: '32' },
+  ];
   hsl: HSL;
 
   constructor(colorService: ColorService) {
     this.colorService = colorService;
     this.hsl = colorService.convertRgbToHsl();
+    this.updateHexList();
+  }
 
-    const MAX = 24;
+  ngOnInit(): void {}
+
+  onSelectNumber() {
+    this.updateHexList();
+  }
+
+  updateHexList() {
+    this.hexList = [];
+    const MAX = Number(this.selected);
     for (let i = 0; i < MAX; i++) {
       const saturation = i * (100 / MAX);
       const rgb = this.colorService.convertHslToRgb({
@@ -26,6 +52,4 @@ export class SaturationComponent implements OnInit {
       this.hexList.push(this.colorService.convertRgbToHex(rgb));
     }
   }
-
-  ngOnInit(): void {}
 }
