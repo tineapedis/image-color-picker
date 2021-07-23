b9115b04f38ce4193eaaff5fc4d041724dbdc130
nmurata <nmurata73@gmail.com>
2021-07-23 19:07:10 +0900

    add color code copy func

--- src/app/screens/chart/components/brightness/brightness.component.ts
+++ src/app/screens/chart/components/brightness/brightness.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ColorService } from 'src/app/services/color.service';
+import { NotificationService } from 'src/app//services/notification.service';
 
 interface ChartNumber {
   value: string;
@@ -32,7 +33,10 @@ export class BrightnessComponent implements OnInit {
   ];
   hsl: HSL;
 
-  constructor(colorService: ColorService) {
+  constructor(
+    colorService: ColorService,
+    private notificationService: NotificationService
+  ) {
     this.colorService = colorService;
     this.hsl = colorService.convertRgbToHsl();
     this.updateChartList();
@@ -60,5 +64,35 @@ export class BrightnessComponent implements OnInit {
         value: lightness.toString(),
       });
     }
+  }
+
+  onClickColorCodeButton(colorCode: string) {
+    this.copyTextToClipboard(colorCode);
+    this.showSnackBar(colorCode);
+  }
+
+  showSnackBar(action: string) {
+    this.notificationService.snackBarSubject.next({
+      message: 'Copy!',
+      action,
+    });
+  }
+
+  // TODO: 何かしらのServiceに持たせる
+  private copyTextToClipboard(text: string) {
+    // テキストエリアを用意する
+    const copyFrom = document.createElement('textarea');
+    // テキストエリアへ値をセット
+    copyFrom.textContent = text;
+    // bodyタグの要素を取得
+    const bodyElm = document.getElementsByTagName('body')[0];
+    // 子要素にテキストエリアを配置
+    bodyElm.appendChild(copyFrom);
+    // テキストエリアの値を選択
+    copyFrom.select();
+    // コピーコマンド発行
+    document.execCommand('copy');
+    // 追加テキストエリアを削除
+    bodyElm.removeChild(copyFrom);
   }
 }
